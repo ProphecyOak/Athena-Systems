3fa0e054778279f3d4c7a00cac2ec1e873c50a54
ProphecyOak <drito610@gmail.com>
2018-03-05 19:18:46 -0500

    Add files via upload

---
+++ Athena (1).py
@@ -0,0 +1,32 @@
+#from time import *
+import time
+print('Welcome to the Athena Network.')
+u = 'User'
+time.sleep(1)
+print('Hello ' + u + ' you can use the command() function to bring up a list of functions that are compatable with the network')
+commands = ['familiarName()', 'athImport()']
+def command():
+    commandLoc = 0
+    print('Right now i can allow you to use these functions')
+    time.sleep(1)
+    while commandLoc < len(commands):
+        print('the ' + commands[commandLoc] + ' function')
+        commandLoc += 1
+        time.sleep(1)
+    
+#def familiarName():
+    #global u = str(input('what would you like me to call you? '))
+    #print('ok, ' + u + ', what would you like me to do now?')
+    
+def athImport():
+    print("ok, for me to import something i need the module's name, and it needs to contain the compatible ATH tah for me to be able to utilize it.")
+    time.sleep(3)
+    print('think of it as giving me notes to study so i can do more cool things for you ^-^')
+    time.sleep(1)
+    #print list of available ones
+    mod = exec(input('what do you want me to import, ' + u + ' , ?'))
+    time.sleep(1)
+    print('Ok, let me get this done. it may take a moment for me to finish it')
+    import mod
+    time.sleep(2)
+    print("there we go, i've imported what you asked for ," + u + '.')
