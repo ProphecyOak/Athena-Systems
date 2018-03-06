caea6b519e30d66ca88b144448a83483941ddac7
Pyromania1162 <37090215+Pyromania1162@users.noreply.github.com>
2018-03-05 19:54:08 -0500

    Update Athena.py

--- Athena.py
+++ Athena.py
@@ -1,9 +1,7 @@
-#from time import *
 import time
-print('Welcome to the Athena Network.')
+
 u = 'User'
-time.sleep(1)
-print('Hello ' + u + ' you can use the command() function to bring up a list of functions that are compatable with the network')
+
 commands = ['familiarName()', 'athImport()']
 def command():
     print('Right now i can allow you to use these functions')
@@ -12,9 +10,12 @@ def command():
         print('the ' + x + ' function')
         time.sleep(1)
     
-#def familiarName():
-    #global u = str(input('what would you like me to call you? '))
-    #print('ok, ' + u + ', what would you like me to do now?')
+def familiarName(x=1):
+    global u
+    u = str(input('what would you like me to call you? '))
+    print('ok, ' + u + ', what would you like me to do now?')
+    if x == 1:
+        print('Ok, ' + u + ', what would you like me to do now?')
     
 def athImport():
     print("ok, for me to import something i need the module's name, and it needs to contain the compatible ATH tah for me to be able to utilize it.")
@@ -24,7 +25,16 @@ def athImport():
     #print list of available ones
     mod = exec(input('what do you want me to import, ' + u + ' , ?'))
     time.sleep(1)
-    print('Ok, let me get this done. it may take a moment for me to finish it')
-    import mod
-    time.sleep(2)
-    print("there we go, i've imported what you asked for ," + u + '.')
+    if str(mod[-4:]) != '_ath':
+        print('hey ' + u + ' I cant read this, im gonna need you to find me a Program with the tag _ath')
+    else:
+        print('Ok, let me get this done. it may take a moment for me to finish it')
+        import mod
+        time.sleep(2)
+        print("there we go, i've imported what you asked for ," + u + '.')
+
+print('Welcome to the Athena Network.')
+time.sleep(1)
+familiarName(0)
+time.sleep(2)
+print('Fyi ' + u + ' You can use the command() function to find other functions im able to use')
