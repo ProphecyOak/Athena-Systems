36059912fd792c8c2b4483871355cd79f84d7703
Pyromania1162 <37090215+Pyromania1162@users.noreply.github.com>
2018-03-06 19:01:51 -0500

    Add files via upload
    update to athena.py

--- Athena.py
+++ Athena.py
@@ -1,9 +1,6 @@
-
-
 from GUI_ath import*
 
 u = 'User'
-
 
 def command():
     Talk('Right now i can allow you to use these functions',1)
@@ -18,24 +15,24 @@ def familiarName(x=1):
     if x == 1:
         Talk('Ok, ' + u + ', what would you like me to do now?')
     
-    
 def athImport():
-    print("ok, for me to import something i need the module's name, and it needs to contain the compatible ATH tah for me to be able to utilize it.")
+    Talk("ok, for me to import something i need the module's name, and it needs to contain the compatible ATH tah for me to be able to utilize it.")
     time.sleep(3)
-    print('think of it as giving me notes to study so i can do more cool things for you ^-^')
+    Talk('think of it as giving me notes to study so i can do more cool things for you ^-^')
     time.sleep(1)
     #print list of available ones
     mod = input('what do you want me to import, ' + u + ' , ?')
     time.sleep(1)
     if str(mod[-4:]) != '_ath':
-        print('hey ' + u + ' I cant read this, im gonna need you to find me a Program with the tag _ath')
+        Talk('hey ' + u + ' I cant read this, im gonna need you to find me a Program with the tag _ath')
     else:
-        print('Ok, let me get this done. it may take a moment for me to finish it')
-        import exec(mod)
+        Talk('Ok, let me get this done. it may take a moment for me to finish it')
+        exec("from "+mod+" import*")
+        user = u
+        print("-imported-")
         time.sleep(2)
-        print("there we go, i've imported what you asked for ," + u + '.')
+        Talk("there we go, i've imported what you asked for ," + u + '.')
 
-    
 Talk('Welcome to the Athena Network.',1)
 familiarName(0)
 time.sleep(1)
