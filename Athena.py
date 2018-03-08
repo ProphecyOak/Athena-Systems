4e18389a5081a08d0ee2aad1f63844d8158a9836
ProphecyOak <drito610@gmail.com>
2018-03-08 17:01:16 -0500

    Update Athena.py

--- Athena.py
+++ Athena.py
@@ -1,37 +1,28 @@
 from GUI_ath import*
 import GlobalFile
-u = 'User'
 
 def command():
-    Talk('Right now i can allow you to use these functions',1)
-    time.sleep(2)
-    Talk('The familiarName() function',1)
-    time.sleep(2)
-    Talk('The athImport() function')
+    Talk('Right now I can allow you to use these functions:',1)
+    Talk('The familiarName() function   -Changes user name',1)
+    Talk('The athImport() function  -Imports additional Athena functionality')
 
 def familiarName(x=1):
-    GlobalFile.User = str(input('what would you like me to call you?\n>>> '))
+    GlobalFile.User = str(input('What would you like me to call you?\n>>> '))
     if x == 1:
-        Talk('Ok, ' + GlobalFile.User + ', what would you like me to do now?')
+        Talk('Ok, ' + GlobalFile.User + ', What would you like me to do now?')
     
 def athImport():
-    print("ok, for me to import something i need the module's name, and it needs to contain the compatible ATH tah for me to be able to utilize it.")
-    time.sleep(3)
-    print('think of it as giving me notes to study so i can do more cool things for you ^-^')
-    time.sleep(1)
-    #print list of available ones
-    mod = input('what do you want me to import, ' + GlobalFile.User + ' , ?')
-    time.sleep(1)
+    print("Ok, for me to import something I need the module's name, and it needs to contain the compatible '_ath' tag for me to utilize it.")
+    print('Think of it as giving me notes to study so I can do more cool things for you. ^-^')
+    mod = input('What do you want me to import, ' + GlobalFile.User + '?\n>>> ')
     if str(mod[-4:]) != '_ath':
-        print('hey ' + GlobalFile.User + ' I cant read this, im gonna need you to find me a Program with the tag _ath')
+        print('Sorry ' + GlobalFile.User + ", I cant read this, I'm gonna need you to find me a program with the tag '_ath'")
     else:
         print('Ok, let me get this done. it may take a moment for me to finish it')
         exec("import "+mod,globals())
         exec("global "+mod,globals())
-        time.sleep(2)
-        print("there we go, i've imported what you asked for ," + GlobalFile.User + '.')
+        print("There we go, I've imported what you asked for, " + GlobalFile.User + '.')
 
 Talk('Welcome to the Athena Network.',1)
 familiarName(0)
-time.sleep(1)
-Talk('Hello ' + GlobalFile.User + ' you can use the command() function to bring up a list of functions that are compatable with the network')
+Talk('Hello ' + GlobalFile.User + ', you can use command() to bring up a list of functions that are compatable with the network')
